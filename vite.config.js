502649403bef029788c291770db9d02c4dbd4bd8
Jerry Ji <1844884543@qq.com>
2026-03-02 00:41:01 -0500

    Update build output and finalize UI

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/individual-project1/',
   plugins: [react()],
+  base: "./",
   build: {
-    outDir: 'docs',
+    outDir: "docs",
     emptyOutDir: true,
   },
-})
+});
